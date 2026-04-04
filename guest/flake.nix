9045e20f1368754b7203c78aeef8d4253b47766b
kncos <kevinncosby2@gmail.com>
2026-04-03 21:21:44 -0400

    wip: new firecracker bundle generation method

--- guest/flake.nix
+++ guest/flake.nix
@@ -9,34 +9,27 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      firecrackerKernel = (import ./targets/kernel.nix { inherit pkgs; }).firecrackerKernel;
+      lib = pkgs.lib;
     in
     {
-      nixosModules = {
-        default = ./configuration.nix;
-        firecracker = ./targets/firecracker.nix;
-        isolate = ./isolate.nix;
-      };
-
-      nixosConfigurations.my-guest = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          self.nixosModules.default
-          self.nixosModules.firecracker
-          self.nixosModules.isolate
-          { nixpkgs.hostPlatform = system; }
-        ];
-      };
-
       packages.${system} = {
         # Accessing the build artifacts directly from the evaluated config
         # rootfs-tar = self.nixosConfigurations.my-guest.config.system.build.tarball;
 
         # This is cleaner: The 'bundle' logic is now a standalone package
         # that takes the 'toplevel' (the built system) as an input.
-        firecracker-bundle = pkgs.callPackage ./targets/bundle.nix {
-          nixosConfig = self.nixosConfigurations.my-guest.config;
-          inherit firecrackerKernel;
+        firecracker-bundle = pkgs.symlinkJoin {
+          name = "firecracker-bundle";
+          paths = [
+            (import ./firecracker/disk-image.nix)
+            {
+              inherit pkgs lib;
+            }
+            (import ./firecracker/vm-config.nix)
+            { inherit pkgs lib; }
+            (import ./firecracker/kernel.nix)
+            { inherit pkgs lib; }
+          ];
         };
 
         default = self.packages.${system}.firecracker-bundle;
