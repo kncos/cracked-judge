839a25a93932639aef0aafe76a15f329e50774d5
kncos <kevinncosby2@gmail.com>
2026-05-08 02:20:02 -0400

    feat: job dependencies finished

--- cracked-judge/apps/guest/src/index.ts
+++ cracked-judge/apps/guest/src/index.ts
@@ -1,17 +1,27 @@
 import { WorkerClient } from "@cracked-judge/common/contract";
+import { env } from "./env";
+import { handleJob } from "./job";
 import { guestLogger } from "./utils";
 
 const main = async () => {
-  using wc = await WorkerClient.create("https://localhost:3000");
+  using wc = await WorkerClient.create(env.JUDGE_SERVER_URL);
   const client = wc.client;
 
   while (true) {
     const start = Date.now();
-    const job = await client.request();
-    if (job === null) {
-      const end = Date.now();
-      guestLogger.debug(`Received null job after ${start - end}ms.`);
-      continue;
+    try {
+      const job = await client.request();
+
+      if (job === null) {
+        const end = Date.now();
+        guestLogger.debug(`Received null job after ${start - end}ms.`);
+        continue;
+      }
+
+      const res = await handleJob(job);
+      await client.submit(res);
+    } catch (e) {
+      guestLogger.error(`Encountered exception: ${e}\nContinuing...`);
     }
   }
 };
